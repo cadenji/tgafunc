b8ac6281d33d9b5361f0234811437df2cc6531bb
cadenji <cadenji@qq.com>
2021-09-05 20:58:59 +0800

    Add create_test() test case

--- test/test.c
+++ test/test.c
@@ -20,46 +20,84 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include "tgafunc.h"
 
-static int image_dimension = 128;
+static void create_test();
 
-static char image_path[] = "test/images/";
-
-static const char *image_names[] =
-{
-    // "CBW8.TGA",      /* Not yet supported. */
-    // "CCM8.TGA",      /* Not yet supported. */
-    // "CTC16.TGA",     /* Not yet supported. */
-    // "CTC24.TGA",     /* Not yet supported. */
-    // "CTC32.TGA",     /* Not yet supported. */
-    "UBW8.TGA",
-    // "UCM8.TGA",      /* Not yet supported. */
-    "UTC16.TGA",
-    "UTC24.TGA",
-    "UTC32.TGA"
-};
-
-static tga_pixel_format pixel_formats[] =
-{
-    // TGA_PIXEL_BW8,
-    // TGA_PIXEL_RGB555,
-    // TGA_PIXEL_RGB555,
-    // TGA_PIXEL_RGB24,
-    // TGA_PIXEL_ARGB32
-    TGA_PIXEL_BW8,
-    // TGA_PIXEL_RGB555,
-    TGA_PIXEL_RGB555,
-    TGA_PIXEL_RGB24,
-    TGA_PIXEL_ARGB32
-};
+static void load_test();
 
 int main(int argc, char *argv[])
 {
-    char image[25];
+    create_test();
+    load_test();
+
+    puts("Test cases passed.");
+    return 0;
+}
+
+static void create_test()
+{
+    tga_image *image_ptr;
+    int max_width_or_height = 65535;
+
+    // image size cannot be less than 1.
+    image_ptr = tga_create(0, 32, TGA_PIXEL_RGB24);
+    assert(image_ptr == NULL);
+    image_ptr = tga_create(32, 0, TGA_PIXEL_RGB24);
+    assert(image_ptr == NULL);
+    // Image size cannot be greater than 65535.
+    image_ptr = tga_create(max_width_or_height + 1, 32, TGA_PIXEL_RGB24);
+    assert(image_ptr == NULL);
+    image_ptr = tga_create(32, max_width_or_height + 1, TGA_PIXEL_RGB24);
+    assert(image_ptr == NULL);
+    // Wrong pixel format check.
+    image_ptr = tga_create(32, 32, (tga_pixel_format)100);
+    assert(image_ptr == NULL);
+
+    image_ptr = tga_create(128, 128, TGA_PIXEL_RGB24);
+    assert(image_ptr != NULL);
+    tga_free(image_ptr);
+}
+
+static void load_test()
+{
+    int image_dimension = 128;
+
+    char image_path[] = "test/images/";
+
+    const char *image_names[] =
+    {
+        // "CBW8.TGA",      /* Not yet supported. */
+        // "CCM8.TGA",      /* Not yet supported. */
+        // "CTC16.TGA",     /* Not yet supported. */
+        // "CTC24.TGA",     /* Not yet supported. */
+        // "CTC32.TGA",     /* Not yet supported. */
+        "UBW8.TGA",
+        // "UCM8.TGA",      /* Not yet supported. */
+        "UTC16.TGA",
+        "UTC24.TGA",
+        "UTC32.TGA"
+    };
+
+    tga_pixel_format pixel_formats[] =
+    {
+        // TGA_PIXEL_BW8,
+        // TGA_PIXEL_RGB555,
+        // TGA_PIXEL_RGB555,
+        // TGA_PIXEL_RGB24,
+        // TGA_PIXEL_ARGB32
+        TGA_PIXEL_BW8,
+        // TGA_PIXEL_RGB555,
+        TGA_PIXEL_RGB555,
+        TGA_PIXEL_RGB24,
+        TGA_PIXEL_ARGB32
+    };
+
     int image_count = sizeof(image_names) / sizeof(image_names[0]);
+    char image[25];
     int loop;
 
     for (loop = 0; loop < image_count; ++loop)
@@ -70,8 +108,7 @@ int main(int argc, char *argv[])
         
         // Load the image and check the file information.
         tga_image *image_ptr = tga_load(image);
-        if (image_ptr == NULL)
-            break;
+        assert(image_ptr != NULL);
         if (image_ptr->width != image_dimension ||
             image_ptr->height != image_dimension ||
             image_ptr->pixel_format != pixel_formats[loop])
@@ -82,10 +119,5 @@ int main(int argc, char *argv[])
         tga_free(image_ptr);
     }
 
-    if (loop == image_count)
-        puts("Load image test passed.");
-    else
-        puts("Load image test failed.");
-
-    return 0;
+    assert(loop == image_count);
 }
